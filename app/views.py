78562691ed24a38a0f515776ca4ae3a89be7e4f0
Surya <surya.sendhilraj@gmail.com>
2021-08-28 16:01:32 -0400

    working database

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Tribe
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "app/index.html")
+    return render(request, "app/index.html", {"tribes": Tribe.objects.all()})
