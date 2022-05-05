287969b4fb7c67250c14f70c779b5abb59ceebd7
Surya Sendhilraj <surya.sendhiraj@gmail.com>
2022-05-04 23:19:03 -0400

    fix login-register glitch

--- register/views.py
+++ register/views.py
@@ -7,6 +7,7 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            # Saves user in user DB
             form.save()
         return redirect("/")
     else:
